e93741dcdc2a36faa555006a1fb9b346b1ed7f40
gemilandu <gemilandu@gmail.com>
2021-03-14 10:37:52 +0100

    Implemented user authorization

--- audioserver_project/settings.py
+++ audioserver_project/settings.py
@@ -37,11 +37,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third party apps
+    # Third-party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
     #My apps
     'audiofiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
